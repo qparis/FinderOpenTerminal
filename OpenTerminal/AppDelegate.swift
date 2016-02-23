e976e61aa93f0f1f9e9d8441149f06eebcbc65b3
Quentin PÂRIS <git@quentin.paris>
2016-02-23 15:55:38 +0100

    Initial Commit

---
+++ OpenTerminal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OpenTerminal
+//
+//  Created by Quentin PÂRIS on 23/02/2016.
+//  Copyright © 2016 QP. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
